7674efae7e803014e90f7ad333b75e0370d25690
seb26 <unleashed26@gmail.com>
2011-08-31 18:40:52 +1000

    major revisions - moved everything into a central object 'tf2extract', containing the config, and all functions. functions now no longer require their own convoluted arguments and instead just read straight from self.config values.

--- tf2-extract/tf2-extract.py
+++ tf2-extract/tf2-extract.py
@@ -7,9 +7,11 @@ import re
 import shutil
 import wikitools
 import hlextract
-
+import wikiUpload
 from collections import defaultdict
-import testpyconfig as wconfig
+
+import wconfig
+wconfig = wconfig.config['tfwiki']
 
 
 def u(s):
@@ -46,148 +48,7 @@ class Re(object):
         return self.last_match
 
 
-class files:
-
-    def __init__(self, package):
-        self.package = package
-        pass
-
-
-    def extract(self, outdir, extr, multidir=True):
-        self.outdir = outdir
-        HL = hlextract.HLExtract(volatile=True)
-        HL.extract(self.package, outdir, extr, multidir=multidir)
-
-
-    def recode(self):
-        """Duplicates files, and recode them to utf-8."""
-        txt = []
-        fdir = os.path.join(self.outdir, 'team fortress 2 content.gcf', r'root\tf\resource')
-        for line in os.listdir(fdir):
-            if re.search(r'\.txt', line):
-                txt.append(line)
-        for f in txt:
-            utf = os.path.join(fdir, 'UTF-8')
-            if not os.path.exists(utf):
-                os.mkdir(utf)
-            src = os.path.join(fdir, f)
-            dst = os.path.join(fdir, 'UTF-8', f)
-            if os.path.isfile(dst):
-                os.remove(dst)
-            shutil.copy2(src, dst)
-            print f, 'Reading...'
-            utf_f = open(dst, 'rb').read(-1).decode('utf16').encode('utf8')
-            print f, 'Writing...'
-            utf_fn = open(dst, 'wb').write(utf_f)
-
-
-    def __upload__(f, name, overwrite=False, reupload=False, summary=None):
-        uploader = wikiUpload.wikiUploader(config.username, config.password, config.url)
-        try:
-            print 'upload():', f, 'uploading to', name, '...'
-            uploader.upload(f, name, summary, overwrite=overwrite, reupload=reupload)
-            print 'Attempted upload of %s | %s\n' % (f, name)
-        except:
-            print 'upload():', f, 'to', name, 'failed.'
-            print 'error:upload() %s | %s\n' % (f, name)
-
-
-    def upload(self, filelist, fdir, summ):
-        for f in filelist:
-            fpath = os.path.join(fdir, f)
-            self.__upload__(fpath, f.capitalize(), overwrite=True, reupload=True, summary=summ)
-            print fpath, f.capitalize(), summ
-
-
-class diff:
-
-    def __init__(self, difffile, patch, files):
-        self.difffile = difffile
-        self.patch = u(patch)
-        self.files = files
-
-        self.template = u'''<!-- This page is updated by a bot. Changes made to it will likely be lost. -->
-== Recent changes ==
-{{tf diff|p=%s}}
-%s
-</div>
-* [{{fullurl:{{FULLPAGENAMEE}}|action=history}} \'''Previous changelogs\''']
-== Licensing ==
-{{Externally linked}}
-{{ExtractTF2}}
-[[Category:Text files]]'''
-
-
-    def format(self):
-        gre = Re()
-        r_add = re.compile(r'^\+[^\+](.+)', re.MULTILINE)
-        r_rem = re.compile(r'^\-[^\-](.+)', re.MULTILINE)
-        r_head = re.compile(r'^@@(.+)', re.MULTILINE)
-        r_con = re.compile(r'^\s[^\}](.+)', re.MULTILINE)
-        r_diff = re.compile(r'^diff --git (a\/.*)\s(b\/.*)', re.MULTILINE)
-
-        f = open(self.difffile, 'r')
-        output = []
-        print 'Parsing %s and formatting...' % self.difffile
-        for line in f.readlines():
-            if gre.match(r_add, line):
-                output.append(u'{{tf diff|+|2=%s}}' % u(gre.match(r_add, line).groups(0)[0]))
-            elif gre.match(r_rem, line):
-                output.append(u'{{tf diff|-|2=%s}}' % u(gre.match(r_rem, line).groups(0)[0]))
-            elif gre.match(r_head, line):
-                output.append(u'{{tf diff|@|2=@@%s}}' % u(gre.match(r_head, line).groups(0)[0]))
-            elif gre.match(r_con, line):
-                output.append(u'{{tf diff|c|2=%s}}' % u(gre.match(r_con, line).groups(0)[0]))
-            elif gre.match(r_diff, line):
-                p = gre.match(r_diff, line).groups(1)[0]
-                output.append('#' + os.path.split(p)[-1])
-            elif line.startswith('index'):
-                pass
-            elif line.startswith('+++') or line.startswith('---'):
-                pass
-            elif line.startswith(' }'):
-                pass
-            elif line.startswith('Binary files'):
-                pass
-            else:
-                output.append(line)
-
-        self.formatted = [ x.strip() for x in output if x ] # Blank line and whitespace stripping.
-        print 'Done.'
-
-
-    def edit(self):
-        output = []
-        d = defaultdict(list)
-        print 'Some rearranging...'
-        for item in self.formatted:
-            if item.startswith('#'):
-                hook = item[1:]
-            else:
-                d[hook].append(item)
-        print 'Logging in.'
-        wiki = wikitools.Wiki(wconfig.apiurl)
-        wiki.login(wconfig.username, wconfig.password)
-        print 'Logged in.'
-        print 'Beginning edit iterator. Today I\'ll be editing:', self.files
-        for f in self.files:
-            if len(d[f]) == 0:
-                # If there is no entry for the file, mark that it is unchanged. defaultdict does not
-                # produce ValueErrors in the same way as a normal dict would.
-                text = self.template % (self.patch, u'{{tf diff|c|2=No changes.}}')
-                print f, '> formatted wikitext. No changes.'
-            else:
-                text = self.template % (self.patch, u'\n'.join(d[f]))
-                print f, '> formatted wikitext.'
-            print f, '> editing...'
-            page = wikitools.Page(wiki, 'File:' + f.capitalize())
-            page.edit(text, summary='Updated diff for [[%s]].' % self.patch, bot=1, skipmd5=True)
-            print f, '> done.'
-
-
-# That's the end of that chapter.
-
-class run:
+class tf2extract:
 
     def __init__(self):
         self.config = {}
@@ -291,26 +152,150 @@ class run:
 
         self.config['difflist'] = self.config['uploadlist']
         self.config['diffile'] = r'X:\Sebi\Desktop\testpy\tf2-extract\raw.diff'
-        self.config['patch'] = 'August 23, 2011 Patch'
+        # self.config['patch'] =
 
-        # Quit editing now.
+        self.template = u'''<!-- This page is updated by a bot. Changes made to it will likely be lost the next time it edits. -->
+== Recent changes ==
+{{tf diff|p=%s}}
+%s
+</div>
+* [{{fullurl:{{FULLPAGENAMEE}}|action=history}} \'''Previous changelogs\''']
+== Licensing ==
+{{Externally linked}}
+{{ExtractTF2}}
+[[Category:Text files]]'''
 
 
-    def run(self, extract=False, upload=False, diff=False):
-        f = files(self.config['packagedir'])
+    def extract(self):
+        HL = hlextract.HLExtract(volatile=True)
+        print 'Extracting...'
+        HL.extract(self.config['packagedir'], self.config['outdir'], self.config['extractlist'], multidir=True)
+
+
+    def recode(self):
+        """Duplicates files, and recode them to utf-8."""
+        txt = []
+        fdir = os.path.join(self.config['outdir'], self.config['package'], r'root\tf\resource')
+        for line in os.listdir(fdir):
+            if re.search(r'\.txt', line):
+                txt.append(line)
+        for f in txt:
+            utf = os.path.join(fdir, 'UTF-8')
+            if not os.path.exists(utf):
+                os.mkdir(utf)
+            src = os.path.join(fdir, f)
+            dst = os.path.join(fdir, 'UTF-8', f)
+            if os.path.isfile(dst):
+                os.remove(dst)
+            shutil.copy2(src, dst)
+            print f, 'Reading...'
+            utf_f = open(dst, 'rb').read(-1).decode('utf16').encode('utf8')
+            print f, 'Writing...'
+            utf_fn = open(dst, 'wb').write(utf_f)
+
+
+    def __upload__(self, f, name, summary):
+        uploader = wikiUpload.wikiUploader(wconfig['usr'], wconfig['pwd'], wconfig['url'])
+        try:
+            print 'upload():', f, 'uploading to', name, '...'
+            uploader.upload(f, name, summary, overwrite=True, reupload=True)
+            print 'Attempted upload of %s | %s\n' % (f, name)
+        except:
+            print 'upload():', f, 'to', name, 'failed.'
+            print 'error:upload() %s | %s\n' % (f, name)
+
+
+    def upload(self):
+        # f.upload(self.config['uploadlist'], self.config['uploadpath'], 'Updated patch for [[%s]]' % self.config['patch'])
+        for f in self.config['uploadlist']:
+            filepath = os.path.join(self.config['uploadpath'], f)
+            print filepath, 'to:', f.capitalize(), 'summ:', 'HI!'
+            self.__upload__(filepath, f.capitalize(), '[[%s]]' % self.config['patch'])
+            print 'Uploaded:', filepath
+
+
+    def format(self):
+        gre = Re()
+        r_add = re.compile(r'^\+[^\+](.+)', re.MULTILINE)
+        r_rem = re.compile(r'^\-[^\-](.+)', re.MULTILINE)
+        r_head = re.compile(r'^@@(.+)', re.MULTILINE)
+        r_con = re.compile(r'^\s[^\}](.+)', re.MULTILINE)
+        r_diff = re.compile(r'^diff --git (a\/.*)\s(b\/.*)', re.MULTILINE)
+
+        f = open(self.config['diffile'], 'r')
+        output = []
+        print 'Parsing %s and formatting...' % self.config['diffile']
+        for line in f.readlines():
+            if gre.match(r_add, line):
+                output.append(u'{{tf diff|+|2=%s}}' % u(gre.match(r_add, line).groups(0)[0]))
+            elif gre.match(r_rem, line):
+                output.append(u'{{tf diff|-|2=%s}}' % u(gre.match(r_rem, line).groups(0)[0]))
+            elif gre.match(r_head, line):
+                output.append(u'{{tf diff|@|2=%s}}' % u(gre.match(r_head, line).groups(0)[0]))
+            elif gre.match(r_con, line):
+                output.append(u'{{tf diff|c|2=%s}}' % u(gre.match(r_con, line).groups(0)[0]))
+            elif gre.match(r_diff, line):
+                p = gre.match(r_diff, line).groups(1)[0]
+                output.append('#' + os.path.split(p)[-1])
+            elif line.startswith('index'):
+                pass
+            elif line.startswith('+++') or line.startswith('---'):
+                pass
+            elif line.startswith(' }'):
+                pass
+            elif line.startswith('Binary files'):
+                pass
+            else:
+                output.append(line)
+
+        self.formatted = [ x.strip() for x in output if x ] # Blank line and whitespace stripping.
+        print 'Done.'
+
+
+    def edit(self):
+        output = []
+        d = defaultdict(list)
+        print 'Some rearranging...'
+        for item in self.formatted:
+            if item.startswith('#'):
+                hook = item[1:]
+            else:
+                d[hook].append(item)
+        print 'Logging in.'
+        wiki = wikitools.Wiki(wconfig['url-api'])
+        wiki.login(wconfig['usr'], wconfig['pwd'])
+        print 'Logged in.'
+        print 'Beginning edit iterator. Today I\'ll be editing:', self.config['difflist']
+        for f in self.config['difflist']:
+            if len(d[f]) == 0:
+                # If there is no entry for the file, mark that it is unchanged. defaultdict does not
+                # produce ValueErrors in the same way as a normal dict would.
+                text = self.template % (self.config['patch'], u'{{tf diff|c|2=No changes.}}')
+                print f, '> formatted wikitext. No changes.'
+            else:
+                text = self.template % (self.config['patch'], u'\n'.join(d[f]))
+                print f, '> formatted wikitext.'
+            print f, '> editing...'
+            page = wikitools.Page(wiki, 'File:' + f.capitalize())
+            page.edit(text, summary='Updated diff for [[%s]].' % self.config['patch'], bot=1, skipmd5=True)
+            print f, '> done.'
+
+
+    def run(self, patch, extract=False, upload=False, diffs=False):
+        self.config['patch'] = patch
         if extract is True:
-            f = files(self.config['packagedir'])
-            f.extract(self.config['outdir'], self.config['extractlist'], multidir=True)
-            f.recode()
+            self.extract()
+            self.recode()
         if upload is True:
-            f.upload(self.config['uploadlist'], self.config['uploadpath'])
-        if diff is True:
-            d = diff(self.config['diffile'], self.config['patch'], self.config['difflist'])
-        if extract is False and upload is False and diff is False:
+            self.upload()
+        if diffs is True:
+            self.format()
+            self.edit()
+        if extract is False and upload is False and diffs is False:
             # Oh god ^
             print 'I do nothing?'
             return self.config
 
 
-x = run()
-x.run(extract=True)
+tf = tf2extract()
+tf.run('August 30, 2011 Patch', extract=False, upload=False, diffs=True)
